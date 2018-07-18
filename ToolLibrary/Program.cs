e0ba6af13d514d1a602991c4977b7364342f1fd9
Robert Babayan <robert1234.babayan@gmail.com>
2018-07-17 21:59:36 -0700

    Base

--- ToolLibrary/Program.cs
+++ ToolLibrary/Program.cs
@@ -1,15 +1,94 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ToolLibrary
+namespace UsefulStuff
 {
-    class Program
+    public class Menus
     {
-        static void Main(string[] args)
+        public class Menu
         {
+            public List<MenuItem> MenuItems;
+
+            private int selectedItem;
+            public int SelectedItem
+            {
+                get
+                {
+                    return selectedItem;
+                }
+
+                set
+                {
+                    if (value < MenuItems.Count && value >= 0)
+                    {
+                        MenuItems[SelectedItem].Selected = false;
+                        selectedItem = value;
+                        MenuItems[SelectedItem].Selected = true;
+                    }
+                }
+            }
+
+            public MenuItem DispVertMenu(int selected = 0)
+            {
+                ConsoleKey input;
+                Console.Clear();
+
+                do
+                {
+                    Console.SetCursorPosition(0, 0);
+
+                    foreach (MenuItem item in MenuItems)
+                    {
+                        Console.Write(item.Selected ? ">{0}" : " {0}", item.DisplayName);
+                        Console.WriteLine();
+                    }
+
+                    input = Console.ReadKey().Key;
+
+                    switch (input)
+                    {
+                        case ConsoleKey.UpArrow:
+                            BackSelect();
+                            break;
+                        case ConsoleKey.DownArrow:
+                            NextSelect();
+                            break;
+                        case ConsoleKey.Enter:
+                            return MenuItems[SelectedItem];
+                    }
+
+                } while (input != ConsoleKey.Enter);
+
+
+                return MenuItems[SelectedItem];
+            }
+
+            public void NextSelect()
+            {
+                if (SelectedItem + 1 < MenuItems.Count)
+                {
+                    MenuItems[SelectedItem].Selected = false;
+                    MenuItems[++SelectedItem].Selected = true;
+                }
+            }
+            public void BackSelect()
+            {
+                if (SelectedItem > 0)
+                {
+                    MenuItems[SelectedItem].Selected = false;
+                    MenuItems[--SelectedItem].Selected = true;
+                }
+            }
+        }
+
+        public class MenuItem
+        {
+            public bool Selected { get; set; }
+            public string DisplayName { get; set; }
+            public int ID { get; set; }
+            public string strVal { get; set; }
+            public decimal numVal { get; set; }
         }
     }
 }
+
